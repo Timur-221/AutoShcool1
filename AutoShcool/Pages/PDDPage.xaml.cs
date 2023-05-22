c2cdcc37b7bee5968569f43db87d33d03ff11c22
Shot Alcoholic-Sperm <timurka.islamov.041@outlook.com>
2023-05-22 13:15:57 +0300

    Add project files.

---
+++ AutoShcool/Pages/PDDPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoShcool.Pages
+{
+    /// <summary>
+    /// Interaction logic for PDDPage.xaml
+    /// </summary>
+    public partial class PDDPage : Page
+    {
+        public PDDPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AccNav(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.Navigate(new AccStud());
+        }
+
+        private void ProfNav(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.Navigate(new ProfPage());
+        }
+    }
+}
